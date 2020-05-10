4e6bceea1c1dced35a3e69181071775f53c1b927
Benny <cb.juerchott@gmail.com>
2020-05-10 15:43:44 +0200

    add recipes

--- src/main/java/de/plugandmod/main/Main.java
+++ src/main/java/de/plugandmod/main/Main.java
@@ -1,6 +1,8 @@
 package de.plugandmod.main;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,6 +14,24 @@ public final class Main extends JavaPlugin {
         System.out.println("Hallo Welt");
         PluginManager  pm  = Bukkit.getPluginManager();
         pm.registerEvents(new Joinlistener(), this);
+        ShapedRecipe gem = new ShapedRecipe(Newitem.gem());
+        gem.shape("GEO","EDE","REL");
+        gem.setIngredient('E',Material.EMERALD);
+        gem.setIngredient('G',Material.GOLD_BLOCK);
+        gem.setIngredient('O',Material.OBSIDIAN);
+        gem.setIngredient('D',Material.DIAMOND);
+        gem.setIngredient('L',Material.LAPIS_BLOCK);
+        gem.setIngredient('R',Material.REDSTONE_BLOCK);
+        Bukkit.addRecipe(gem);
+
+
+
+        ShapedRecipe sword = new ShapedRecipe(Newitem.sword());
+        gem.shape("AUA","AUA","ABA");
+        gem.setIngredient('A',Material.AIR);
+        gem.setIngredient('U',Newitem.gem());
+        gem.setIngredient('B',Material.BLAZE_ROD);
+
     }
 
     @Override
--- src/main/java/de/plugandmod/main/Newitem.java
+++ src/main/java/de/plugandmod/main/Newitem.java
@@ -1,15 +1,20 @@
 package de.plugandmod.main;
 
 
-import org.bukkit.inventory.*;
-
-import static org.bukkit.Material.*;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
 public class Newitem {
 
-    public static ItemStack sword{
-        ItemStack a = new ItemStack(BEEF);
+    public static ItemStack gem(){
+        ItemStack a = new ItemStack(Material.DIAMOND);
         return a;
+
+    }
+    public static ItemStack sword(){
+        ItemStack a = new ItemStack(Material.DIAMOND);
+        return a;
+
     }
 
 }
