9e16c44f97645127e6f4e281324c79d1516b936b
Joni135 <jonathan.benjamin.zeller@googlemail.com>
2020-05-10 12:35:50 +0200

    init

---
+++ src/main/java/codedesign/plugmodgit/PlugModGit.java
@@ -0,0 +1,17 @@
+package codedesign.plugmodgit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlugModGit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
